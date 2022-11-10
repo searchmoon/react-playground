a3854f3ba99a658547011821b201ed36a367aa09
searchmoon <moonding901212@gmail.com>
2022-11-11 01:28:58 +0900

    map data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import './App.css';
-import {useCallback, useState, useEffect} from "react";
+import "./App.css";
+import { useCallback, useState, useEffect } from "react";
 import Loading from "./components/Loading";
 import axios from "axios";
+import TodoList from "./components/TodoList";
+
 function App() {
   const [todos, setTodos] = useState(null);
 
-  useEffect(async() => {
-  const todoData = await axios.get('https://jsonplaceholder.typicode.com/todos');
-  console.log(todoData);
-  setTodos(todoData.data);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => setTodos(res.data));
   }, []);
 
-  console.log('todos', todos)
+  console.log("todos", todos);
   return (
     <>
       {/*{todos ? <div>todos</div> : <Loading />}*/}
-      {todos? {todos.map((list) => <div key={list.id}>{list.title}</div>)} : <Loading />}
+      {/* {todos? {todos.map((list) => <div key={list.id}>{list.title}</div>)} : <Loading />} */}
+      {todos ? <TodoList todos={todos} /> : <Loading />}
     </>
   );
 }
