853a83636202d60793e8a9e12645534f1a0e359d
Rumi <moon901212@gmail.com>
2022-11-16 23:55:21 +0900

    패키지 설치

--- src/App2.js
+++ src/App2.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Login from './components/LogIn';
 import Example1 from './Example1';
 import Example2 from './Example2';
 
 function App2() {
-	return <Example2 />;
+	return <Login />;
 }
 
 export default App2;
